4dd6b8bf1c9e27810d7811b079a2fe6350a0c59a
cloudera <cloudera@quickstart.cloudera>
2017-04-20 20:39:22 -0700

    add job config in run

--- src/stripeApproach/StripeMain.java
+++ src/stripeApproach/StripeMain.java
@@ -1,15 +1,21 @@
 package stripeApproach;
 
 import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.MapWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
-
+import org.apache.hadoop.util.ToolRunner;
 
 
 public class StripeMain extends Configured implements Tool  {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
+		int res = ToolRunner.run(new StripeMain(), args);
+		System.exit(res);
 
 	}
 
@@ -17,7 +23,14 @@ public class StripeMain extends Configured implements Tool  {
 	public int run(String[] arg0) throws Exception {
 		Job job = Job.getInstance(getConf(), "StripeJob");
 		job.setJarByClass(this.getClass());
-	return 0;
+		FileInputFormat.addInputPath(job, new Path(arg0[0]));
+		FileOutputFormat.setOutputPath(job, new Path(arg0[1]));
+		job.setMapperClass(StripeMapper.class);
+		job.setReducerClass(StripeReducer.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(MapWritableExtended.class);
+		job.setMapOutputValueClass(MapWritable.class);
+		return job.waitForCompletion(true) ? 0 : 1;
 	}
 
 }
